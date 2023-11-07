72b8dc7e82569d1cca83aa975d75364157d6bf1b
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-07 17:09:43 +0100

    feat: terminato

--- js/main.js
+++ js/main.js
@@ -1,32 +1,81 @@
 'use strict';
 
-//bottone
-const bottone = document.querySelector('button');
+//bottoni
+const bottone = document.getElementById('genera');
+const bottoneReset = document.getElementById('reset');
 
-//evento del bottone
+ // valore elementi input
+ const nomeCognome = document.getElementById('nome-cognome');
+ const km = document.getElementById('km');
+ const eta = document.getElementById('eta').value;
+
+//evento del bottone genera
 bottone.addEventListener('click',
     function(){
-        // valore elementi input
-        const km = document.getElementById('km').value;
-        const eta = document.getElementById('eta').value;
-
         //prezzo
-        let prezzo = km * 0.21;
+        let prezzo = km.value * 0.21;
         let sconto = 0;
+        let testoSconto = 'Biglietto Standard';
 
         //sconto
-        if(eta < 18){
+        if(eta === 'minorenne'){
             sconto = 20;
-        }else if(eta >= 65){
+            testoSconto = 'Biglietto Minorenne';
+        }else if(eta === 'over65'){
             sconto = 40;
+            testoSconto = 'Biglietto Over 65';
         }
         prezzo -= ((prezzo / 100) * sconto);
 
         //arrotonda a due cifre decimali
-        const testo = 'Prezzo: ' + prezzo.toFixed(2) + '€';
+        const testo = prezzo.toFixed(2) + '€';
 
-        //output
-        console.log(testo);
-        document.getElementById('risultato').innerHTML = testo;
+        /* output */
+        if(nomeCognome.value !== '' && km.value !== ''){
+            //se i campi nome e km non sono vuoti
+            document.getElementById('nome').innerHTML = nomeCognome.value;
+            document.getElementById('sconto').innerHTML = testoSconto;
+            document.getElementById('numeroCarrozza').innerHTML = Math.floor(Math.random() * 30);
+            document.getElementById('numeroCP').innerHTML = Math.floor(Math.random() * 10000);
+            document.getElementById('prezzo').innerHTML = testo;
+            const biglietto = document.getElementById('biglietto');
+            biglietto.classList.remove('d-none');
+            biglietto.classList.add('d-flex');
+            document.getElementById('errore').classList.add('d-none');
+        }else{
+            //se sono vuoti
+            document.getElementById('errore').classList.remove('d-none');
+            biglietto.classList.remove('d-flex');
+            biglietto.classList.add('d-none');
+
+        }
+    }
+);
+
+//evento del bottone reset
+bottoneReset.addEventListener('click',
+    function(){
+        //rimuove d-block e mette d-none
+        const biglietto = document.getElementById('biglietto');
+        biglietto.classList.remove('d-flex');
+        biglietto.classList.add('d-none');
+        //aggiunge l'attributo disabled
+        bottone.setAttribute('disabled', true);
+    }
+);
+
+// se sono presenti nome e km toglie l'attributo disabled dal bottone
+nomeCognome.addEventListener('keypress',
+    function(){
+        if(km.value !== ''){
+            bottone.removeAttribute('disabled');
+        }
+    }
+);
+km.addEventListener('keypress',
+    function(){
+        if(nomeCognome.value !== ''){
+            bottone.removeAttribute('disabled');
+        }
     }
 );
